8cfee9d35fa057df4cb62fd1ba44b52f11983a77
lammi1 <67054161+lammi1@users.noreply.github.com>
2020-07-24 19:05:58 +0900

    Create 3-2_buggy1.c

---
+++ 3-2_buggy1.c
@@ -0,0 +1,13 @@
+//buggy example for printf and debug50
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("#\n");
+    }
+}
+
+in CS50 IDE
+we can use debug50 ./buggy1.c
